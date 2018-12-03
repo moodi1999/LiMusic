7869508543819a528b6a535ad9a3a4e43069a73c
Ahmadreza Moodi <moodi1378@gmail.com>
2018-12-03 21:51:37 +0330

    complete GetSongPageContent now we can bring the docs in to our app

--- app/src/main/java/com/moodi/limusic/GetPageContent.kt
+++ app/src/main/java/com/moodi/limusic/GetPageContent.kt
@@ -1,22 +1,69 @@
 package com.moodi.limusic
 
 import android.os.AsyncTask
+import android.util.Log
+import java.io.BufferedReader
+import java.io.FileNotFoundException
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+import java.net.UnknownHostException
 
 interface GetPageContentCallback {
     fun onGetPageCompelet(content: String)
     fun onGetPageFailed()
 }
-class GetPageContent(mCallback: GetPageContentCallback) : AsyncTask<String, Unit, String>() {
+
+class GetPageContent(val mCallback: GetPageContentCallback) : AsyncTask<String, Unit, String>() {
+    private val TAG = "GetPageContent"
+    private val D = false
+
+    private val FAILED = "Failed"
+
+    override fun doInBackground(vararg urls: String?): String {
+        val content = StringBuilder()
+
+        try {
+            val connection = URL(urls[0]).openConnection() as HttpURLConnection
+            val reader = BufferedReader(InputStreamReader(connection.inputStream))
+            var readChars: Int
+            val inputBuffer = CharArray(500)
+            while (true) {
+                readChars = reader.read(inputBuffer)
+
+                if (readChars < 0) {
+                    break
+                }
+                if (readChars > 0) {
+                    content.append(String(inputBuffer, 0, readChars))
+                }
+            }
+
+            reader.close()
+            return content.toString()
+
+        } catch (malf: MalformedURLException) {
+            if (D) Log.i(TAG, "doInBackground : malf is :=: ${malf}")
+            mCallback.onGetPageFailed()
+            return FAILED
+        } catch (e: FileNotFoundException) {
+            if (D) Log.i(TAG, "doInBackground : FileNotFoundException is :=: ${e}")
+            return FAILED
+        } catch (e: UnknownHostException) {
+            if (D) Log.i(TAG, "doInBackground : UnknownHostException is :=: ${e}")
+            return FAILED
+        } catch (e: Exception) {
+            if (D) Log.i(TAG, "doInBackground : Exception is :=: ${e}")
+            return FAILED
+        }
+    }
 
     override fun onPostExecute(result: String?) {
         super.onPostExecute(result)
+        if (result != FAILED) {
+            mCallback.onGetPageCompelet(result!!)
+        }
     }
 
-    override fun doInBackground(vararg params: String?): String {
-        
-    }
-
-    override fun onPreExecute() {
-        super.onPreExecute()
-    }
 }
--- app/src/main/java/com/moodi/limusic/activities/MainActivity.kt
+++ app/src/main/java/com/moodi/limusic/activities/MainActivity.kt
@@ -2,13 +2,24 @@ package com.moodi.limusic.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.moodi.limusic.GetPageContent
+import com.moodi.limusic.GetPageContentCallback
 import com.moodi.limusic.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , GetPageContentCallback{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GetPageContent(this).execute("http://printatesfasfasfsdafstpage.com/")
+    }
+
+    override fun onGetPageCompelet(content: String) {
+        println(content)
+    }
+
+    override fun onGetPageFailed() {
 
     }
 }
