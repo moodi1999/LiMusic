423872716ca1a751551aa1e002f63fa2e581c82d
Ahmadreza Moodi <moodi1378@gmail.com>
2018-12-19 16:02:24 +0330

    recycler view items changed to material and added picasso for photos

--- app/src/main/java/com/moodi/limusic/View/Adaptors/SongRecyAdp.kt
+++ app/src/main/java/com/moodi/limusic/View/Adaptors/SongRecyAdp.kt
@@ -7,13 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import com.moodi.limusic.R
 import com.moodi.limusic.model.data.Song
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.recycler_view_item.view.*
 import kotlinx.android.synthetic.main.song_recy_item.view.*
 import java.text.FieldPosition
 
 class SongRecyAdp(val context: Context, val songs: ArrayList<Song>) : RecyclerView.Adapter<SongRecyAdp.SViewHolder>() {
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): SViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.song_recy_item, p0, false)
+        val view = LayoutInflater.from(p0.context).inflate(R.layout.song_recy_item, p0, false)
         return SViewHolder(view)
     }
 
@@ -24,11 +26,24 @@ class SongRecyAdp(val context: Context, val songs: ArrayList<Song>) : RecyclerVi
     override fun onBindViewHolder(viewh: SViewHolder, position: Int) {
         val song = songs[position]
         viewh.title.text = song.title
+        viewh.views.text = song.views
+        viewh.cardView.setOnClickListener {
+            println("song clicked")
+        }
+        println(song.imgUrl)
+        Picasso.with(context!!).load(song.imgUrl).into(viewh.songImage)
     }
 
     inner class SViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val cardView = itemView.song_card_view
         val title = itemView.song_title
-        val view = itemView
+        val views = itemView.song_views
+        val songImage = itemView.song_image_view
+        init {
+            cardView.preventCornerOverlap = false
+            itemView.song_image_card.bringToFront()
+            itemView.song_image_view.bringToFront()
+        }
     }
 
 }
